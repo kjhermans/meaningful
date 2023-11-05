7a28980069340dba84ad6660c3faeee24de939ef
Kees-Jan Hermans <kees.jan.hermans@gmail.com>
2023-11-05 11:59:19 +0100

    More tests and more docs.

--- tester.sh
+++ tester.sh
@@ -2,10 +2,21 @@
 
 TESTS=`ls test_*.json | sort`
 for TEST in $TESTS; do 
+  echo "-----"
   echo $TEST
   cat $TEST
   perl ./encoder.pl $TEST > $TEST.enc
   hexdump -C $TEST.enc
+  perl ./decoder.pl $TEST.enc > $TEST.out
+  cat $TEST.out
+done
+
+TESTS=`ls test_*.hex | sort`
+for TEST in $TESTS; do 
+  echo "-----"
+  echo $TEST
+  cat $TEST
+  xxd -r -p $TEST $TEST.enc
   perl ./decoder.pl $TEST.enc > $TEST.out
   cat $TEST.out
 done
